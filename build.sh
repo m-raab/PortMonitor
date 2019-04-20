e13b88fab8a87e8621e013ab64385a132175ab5d
Matthias Raab <m.raab@intershop.de>
2019-04-20 23:01:44 +0200

    build script optimized

--- build.sh
+++ build.sh
@@ -10,14 +10,14 @@ do
     platform_split=(${platform//\// })
     GOOS=${platform_split[0]}
     GOARCH=${platform_split[1]}
-    output_name=bin/$GOOS-$GOARCH/$package_name
+    output_name=bin/${GOOS}-${GOARCH}/${package_name}
 
-    env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_name $package
+    env GOOS=${GOOS} GOARCH=${GOARCH} go build -o ${output_name} ${package}
     if [ $? -ne 0 ]; then
         echo 'An error has occurred during GO compilation! Aborting the script execution...'
         exit 1
     fi
-    mkdir -p dist && tar -zcvf dist/$package_name.$GOOS.$GOARCH.tar.gz bin/$GOOS-$GOARCH/$package_name
+    mkdir -p dist && tar -zcvf dist/${package_name}.${GOOS}.${GOARCH}.tar.gz bin/${GOOS}-${GOARCH}/${package_name}
     if [ $? -ne 0 ]; then
         echo 'An error has occurred during packaging! Aborting the script execution...'
         exit 1
